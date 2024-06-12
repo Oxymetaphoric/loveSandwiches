eeb32ea9b7336106b8a4b0415eb950d916fad360
Oxymetaphoric <zombieplayground@gmail.com>
2024-06-12 14:46:12 +0100

    BUILD: update worksheet function

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -19,22 +20,39 @@ data = sales.get_all_values()
 print(data)
 
 def get_sales_data():
-    print("please enter sales data from the last market.")
-    print("Data should be six numbers, seperated by commas.")
-    print("Example: 10,20,30,40,50,60")
-
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+    while True:
+        print("please enter sales data from the last market: ")
+        print("Data should be six numbers, seperated by commas.")
+        print("Example: 10,20,30,40,50,60")
+              
+    
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+    
+    return sales_data
 
 def validate_data(values):
     try:
         [int(value) for value in values] 
         if len(values) != 6:
             raise ValueError(
-                    f'exactly 6 values required, you provided {len(values)}'
-                    )
+                f'exactly 6 values required, you provided {len(values)}'
+            )
     except ValueError as e:
-        print(f'Invalid data: {e}, please try again.\n')
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
+    return True
+
+def update_sales_worksheet(data):
+    print("Updating worksheet...\n")
+    sales_worksheet=SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("sales worksheet updated successfully!\n")
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
